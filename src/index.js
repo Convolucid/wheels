7bf0b9457025a6d708ee858b9a307052de220ff3
Convolucid <convolucid@gmail.com>
2022-06-17 15:36:00 -0500

    Added CSS file to webpack process

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('initialization successful')
+import './style.css'
+
+console.log('initialization successful');
+
+function component() {
+    const element = document.createElement('div');
+    
+    element.innerHTML = 'Hello world!';
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: 
+    [
+        new HtmlWebpackPlugin({
+            title: 'Initialization'
+        })
+    ],
+    output: 
+    {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        clean: true
+    },
+    module: 
+    {
+        rules: 
+        [
+            // CSS
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+};
