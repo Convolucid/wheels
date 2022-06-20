fb51f059c5673a2943050bb6912bf4059b63d6df
Convolucid <convolucid@gmail.com>
2022-06-20 15:58:31 -0500

    Small text change to test git/Vercel connection

--- src/script.js
+++ src/script.js
@@ -12,4 +12,4 @@ function component(content) {
 }
 
 document.body.appendChild(component(html))
-document.body.appendChild(component('Hello Cheshire Cat!'))
+document.body.appendChild(component('Hello White Rabbit!'))
