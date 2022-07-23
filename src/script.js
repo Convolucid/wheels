7bf4741e9f58c81728aaf6bbbac5f5b503afff5f
Convolucid <convolucid@gmail.com>
2022-07-22 20:29:32 -0500

    Added SVG horizontal scroll and text fade-in

--- src/script.js
+++ src/script.js
@@ -77,30 +77,8 @@ processArticleSection2.innerHTML = processContent2;
 processArticleSection3.innerHTML = processContent3;
 processArticleSection4.innerHTML = processContent4;
 
-// const processArticleSectionArray = [
-//     processArticleSection1,
-//     processArticleSection2,
-//     processArticleSection3,
-//     processArticleSection4
-// ]
 
-
-// Navigation functions: page display function that displays a page of the array and hides other pages, and menu button switches.  
-function displayContent(content, contentArray)
-{
-    for(let i=0; i < contentArray.length; i++)
-    {
-        if(contentArray[i] == content)
-        {
-            contentArray[i].style.display = 'block';
-        }
-        else
-        {
-            contentArray[i].style.display = 'none';
-        }
-    }
-    resize();
-}
+// Navigation functions: page display function that displays a page of the array and hides other pages, and menu button switches. 
 
 function expandMenu()
 {
@@ -150,46 +128,76 @@ function resize()
 }
 resize();
 
-// Event listeners for links and menu button functionality
-window.addEventListener('resize', resize)
 
-homeLink.addEventListener('click', ()=> {
-    displayContent(homeArticle, articleArray)
-    // First attempts at SVG Animation using GSAP
-    moveHomeHeroImage.reverse()
-})
-historyLink.addEventListener('click', ()=> {
-    displayContent(historyArticle, articleArray)
-    // SVG animation
-    moveHomeHeroImage.play()
-})
-processLink.addEventListener('click', ()=> {
-    displayContent(processArticle, articleArray)
-})
-futureLink.addEventListener('click', ()=> {
-    displayContent(futureArticle, articleArray)
-})
+function displayContent(content, contentArray)
+{
+    for(let i=0; i < contentArray.length; i++)
+    {
+        if(contentArray[i] == content)
+        {
+            contentArray[i].classList.remove('main-article-hidden')
+        }
+        else
+        {
+            contentArray[i].classList.add('main-article-hidden')
+        }
+    }
+    resize();
+}
 
-navMenuButton.addEventListener('click', toggleMenuOverlay)
+displayContent(homeArticle, articleArray)
 
-
-
-// SVG manipulation
+// 
+// SVG
+// 
 const svgHomeHeroImage = document.getElementById('aPath')
 const svgHomeContentImage = document.getElementById('bPath')
 const svgIconContainerImage = document.getElementById('cPath')
 
 // Manipulating the first and third values (x and width) allows for interesting zooming and panning.  Reducing the width zooms in by that amount, and then x can be set in that range while still maintaining the svg in the entire field of view.
-svgHomeHeroImage.setAttribute('viewBox', '0 0 1200 1200')
-svgHomeContentImage.setAttribute('viewBox', '0 0 1200 1042')
-svgIconContainerImage.setAttribute('viewBox', '0 0 1200 1042')
+svgHomeHeroImage.setAttribute('viewBox', '0 0 1280 1024')
+svgHomeContentImage.setAttribute('viewBox', '768 0 1224 1024')
+svgIconContainerImage.setAttribute('viewBox', '0 0 1280 1024')
 
-let moveHomeHeroImage = gsap.to(
-    svgHomeContentImage.viewBox.baseVal,
-    1,
-    {
-        x: 200,
-        width: 600,
-        paused: true
-    }
-)
+function moveSVG(svg, xPos) {
+    gsap.to(
+        svg.viewBox.baseVal,
+        {
+            duration: 1.5,
+            x: xPos,
+            ease: 'power4.inOut'
+        }
+    )
+}
+
+
+// 
+// Event listeners for links and menu button functionality.  Includes SVG background image movement with GSAP.
+// 
+window.addEventListener('resize', resize)
+
+navMenuButton.addEventListener('click', toggleMenuOverlay)
+
+homeLink.addEventListener('click', ()=> {
+    displayContent(homeArticle, articleArray)
+    moveSVG(svgHomeHeroImage, 0)
+    moveSVG(svgHomeContentImage, 768)
+})
+
+historyLink.addEventListener('click', ()=> {
+    displayContent(historyArticle, articleArray)
+    moveSVG(svgHomeHeroImage, 192)
+    moveSVG(svgHomeContentImage, 576)
+
+})
+processLink.addEventListener('click', ()=> {
+    displayContent(processArticle, articleArray)
+    moveSVG(svgHomeHeroImage, 384)
+    moveSVG(svgHomeContentImage, 384)
+})
+
+futureLink.addEventListener('click', ()=> {
+    displayContent(futureArticle, articleArray)
+    moveSVG(svgHomeHeroImage, 576)
+    moveSVG(svgHomeContentImage, 192)
+})
